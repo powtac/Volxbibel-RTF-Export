fdfa315ae9f937a56b8a16e7ba9c473fcf50ac1c
Simon Brüchner <powtac@gmx.de>
2012-06-04 18:08:35 +0200

    clean up

--- wikiexport/jesusrockt.php
+++ wikiexport/jesusrockt.php
@@ -2,10 +2,8 @@
 /**
  * Holt die Kapitel von Jesus Rockt und baut einen Download Link zusammen
  */
-header ('Content-type: text/html; charset=utf-8');
+require_once dirname(__FILE__).'/config/bootstrap.php';
 
-require_once dirname(__FILE__).'/config/config.php';
-require_once dirname(__FILE__).'/config/basic.php';
 
 define('CHAPTER_LIST_WIKI_ARTICLE', 'Jesus_Rockt_Die_Kapitel');
 
@@ -32,13 +30,13 @@ foreach ($chapter as $chapt) {
     $raw = unserialize(http(makeUrl($tmp)));
 
     echo '<br>';
-    echo '<a href="http://at.volxbibel.com/index.php/'.$tmp.'">'.$tmp.'</a> ';
+    echo '<a href="'.WIKI_SERVER.'/index.php/'.$tmp.'">'.$tmp.'</a> ';
 
     if (!is_array($raw) OR !isset($raw['query']['pages'][-1]['missing'])) {
         echo ' <span style="color:green;font-weight:bold">Kapitelname OK</span>';
         echo ' <a href="./index.php?page_name='.$tmp.'">Export</a>';
     } else {
-        echo ' <span style="color:red">Kapitelname nicht Ok, bitte checke auf <a href="http://at.volxbibel.com/index.php/'.CHAPTER_LIST_WIKI_ARTICLE.'">der Kapitelliste</a> ob der Eintrag "<strong>'.$chapt.'</strong>" auf den exakt gleich geschrieben Artikel verlinkt und ob er einen Inhalt hat!</span>';
+        echo ' <span style="color:red">Kapitelname nicht Ok, bitte checke auf <a href="'.WIKI_SERVER.'/index.php/'.CHAPTER_LIST_WIKI_ARTICLE.'">der Kapitelliste</a> ob der Eintrag "<strong>'.$chapt.'</strong>" auf den exakt gleich geschrieben Artikel verlinkt und ob er einen Inhalt hat!</span>';
     }
 
 
